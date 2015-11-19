46303982ffa31a01827ae468eb0374b36dab87f1
frank <592761667@qq.com>
2015-11-20 02:46:28 +0800

    while input; count word

--- 1/chp1-3_char.c
+++ 1/chp1-3_char.c
@@ -1,12 +1,35 @@
-#include <stdio.h>
-int main (){
-    int c;
+/* counting of character, line and word
+ * frank
+ * 2015.11.19
+ */
 
-    c = getchar();
-    while(c != EOF){
-        putchar(c);
-        c = getchar();
+#include <stdio.h>
+
+#define IN 1 /* inside a word */
+#define OUT 0 /* outside a word */
+
+int main (int argc, int argv[]){
+    printf("%d\n\n", EOF);// EOF = -1
+
+    int numChar = 0, numLine = 0,  numWord = 0;
+    /* redirect a file*/
+    char s;
+    int state = OUT;
+    while((s = getchar()) != EOF){
+        putchar(s);
+        ++numChar;
+        
+        if(s == '\n')
+            ++numLine;
+        
+        if(s == ' ' || s == '\n' || s == '\t'){
+            state = OUT;
+        }else if (state == OUT){
+            state = IN;
+            ++numWord;  // count when get into a word.
+        }
     }
-    printf("%d\n", EOF);// EOF = -1
+    printf("The number of char, line and words are %d, %d, %d.\n", 
+            numChar, numLine, numWord); 
     return 0;
 }
--- Summary.md
+++ Summary.md
@@ -2,3 +2,28 @@ The experience obtained from coding.
 
 1. **Problem**: `$ gcc x.c` does not compile right if x.c use function cos(). But acos() works.  
    **Partial solution**: "gcc x.c -lm" link with math.h library while compile. 
+2. **Problem**: chp1-3_char.c
+    ```
+    while((s = getchar()) != EOF){
+        putchar(s);
+        ++numChar;
+        
+        if(s == '\n')
+            ++numLine;
+        
+        if(s == ' ' || s == '\n' || s == '\t'){
+            state = OUT;
+            ++numWord;
+        }else if (state == OUT){
+            state = IN;
+            //++numWord;
+        }
+    }
+    ``` 
+    The result is as follows, when I input "abcd <enter>" or "abcd <ctrl+d>": 
+    ```
+    abcd
+    abcd
+
+    ```
+    Input maybe implemented with a queue. Input to a queue, <enter> or <ctrl+d> as an end. Then the queue is poped to the program.
